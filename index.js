a8a17b893dd75c1e6658783152f3ee809875b54d
Ville Saukkonen <ville@smartly.io>
2017-12-16 00:00:55 +0200

    flow warning issue

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // @flow
-import untyped from 'react-redux';
 
-untyped(function(): number {
-  const thisIsNotCovered: any = 'some text';
-  const thisLineIsCovered: number = 123;
-  const thisLineIsCoveredToo = thisLineIsCovered;
-  return thisLineIsCoveredToo;
-});
+// $FlowFixMe
+const num = 123;
+
+const numb: number = '123';
